714d8c8668a935bfb4c26d12a526d46b78b3f399
Ben Cohen <ben.cohenvi@gmail.com>
2019-04-18 21:28:12 +0300

    solved problem

--- IsNumberSumInArray/IsNumberSumInArray/Program.cs
+++ IsNumberSumInArray/IsNumberSumInArray/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace IsNumberSumInArray
 {
-    class Program
+    public class Program
     {
+        public static bool IsKSumInArray(int[] arr, int k)
+        {
+            foreach (int number in arr)
+            {
+                if (arr.Contains(k - number))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
         static void Main(string[] args)
         {
+            int[] arr = { 10, 15, 3, 7 };
+            Console.WriteLine(IsKSumInArray(arr, 26));
+            Console.ReadKey();
         }
     }
 }
