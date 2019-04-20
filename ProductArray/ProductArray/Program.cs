1f22b0e709078e7cd43f27fb53b4b4b7ebdbb392
Ben Cohen <ben.cohenvi@gmail.com>
2019-04-20 12:28:56 +0300

    Created a basic structure for the project

---
+++ ProductArray/ProductArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductArray
+{
+    class Program
+    {
+        public static int[] ProductOfOtherItems(int[] arr)
+        {
+            int[] productArray = new int[arr.Length];
+            return productArray;
+        }
+
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
